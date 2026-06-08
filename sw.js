0091892cdd1c383bc2dd0a1ff7e2050c5a52f1b2
motilmchef <motilm@gmail.com>
2026-06-08 20:03:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gan-sipur-v' + '2025061001';
+const CACHE = 'gan-sipur-v' + '2026060801';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
